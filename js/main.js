956210f8ad45bbca86df97b0c17ae9f3bf7ef428
unknown <nicolasfediuk@gmail.com>
2022-07-24 19:51:57 -0300

    validacion form

--- js/main.js
+++ js/main.js
@@ -5,43 +5,97 @@ const nombre = document.querySelector(".form-nombre");
 const email = document.querySelector(".form-email");
 const mensaje = document.querySelector(".form-mensaje");
 const btn = document.getElementById('button');
+const p = document.querySelector(".mensajeError")
 
 cargarEvento();
 
 function cargarEvento() {
     btnNav.addEventListener("click", mostrarNav);
-    // nombre.addEventListener("blur", validarFormulario);
-    // email.addEventListener("blur", validarFormulario);
-    // mensaje.addEventListener("blur", validarFormulario);
+    nombre.addEventListener("blur", validarFormulario);
+    email.addEventListener("blur", validarFormulario);
+    mensaje.addEventListener("blur", validarFormulario);
+    btn.addEventListener("click",enviarFormulario);
 }
 
 const validarCorreo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-const validarNombre = /^[A-Z]+$/i;
-console.log(btn);
+const validarNombre = /^[a-zA-Z ]+$/;
+
+function validarFormulario(e) {
+
+
+    if (validarNombre.test(nombre.value)) {
+        nombre.style.borderBottom = "1px solid green";
+    }
+    else {
+        nombre.style.borderBottom = "1px solid red";
+    }
 
 
 
-    btn.style.cursor = "pointer";
-    document.getElementById('form').addEventListener('submit', function (event) {
-        event.preventDefault();
-    
-        btn.value = 'Enviando...';
-    
-        const serviceID = 'default_service';
-        const templateID = 'template_cbw2wut';
-    
-        emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            btn.value = 'Enviar';
-            alert('Mensaje enviado!');
-            nombre.value = "";
-            email.value = "";
-            mensaje.value = "";
-        }, (err) => {
-            btn.value = 'Mensaje enviado';
-            alert(JSON.stringify(err));
+    if (validarCorreo.test(email.value)) {
+        email.style.borderBottom = "1px solid green";
+    }
+    else {
+        email.style.borderBottom = "1px solid red";
+    }
+
+
+    if(mensaje.value != ""){
+        mensaje.style.borderBottom = "1px solid green";
+    }
+    else{
+        mensaje.style.borderBottom = "1px solid red";
+    }
+}
+
+
+
+function enviarFormulario(){
+    if(validarCorreo.test(email.value) && validarNombre.test(nombre.value) && mensaje.value != ""){
+
+        document.getElementById('form').addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            btn.value = 'Enviando...';
+
+            const serviceID = 'default_service';
+            const templateID = 'template_cbw2wut';
+
+            emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Enviar';
+                Swal.fire({
+                    title: 'Mensaje enviado',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                });
+                borrarCampos();
+            }, (err) => {
+                btn.value = 'Mensaje enviado';
+                alert(JSON.stringify(err));
+            });
         });
-    });
+        
+    }
+    else{
+        p.textContent="Verifique los datos ingresados";
+    }
+}
+
+function borrarCampos(){
+    nombre.value = "";
+    nombre.style.borderBottom = "1px solid black";
+    email.value = "";
+    email.style.borderBottom = "1px solid black";
+    mensaje.value = "";
+    mensaje.style.borderBottom = "1px solid black";
+    p.textContent = "";
+}
+
+
+
+
+    
     
 
 
@@ -60,27 +114,4 @@ window.onscroll = () => {
     nav.style.display = 'none';
 }
 
-// function validarFormulario(e) {
-//     if (e.target.type === "text") {
-//         if (validarNombre.test(e.target.value)) {
-//             e.target.style.borderBottom = "1px solid green";
-//         }
-//         else {
-//             e.target.style.borderBottom = "1px solid red";
-//         }
-//     }
-//     if (e.target.type === "email") {
-//         if (validarCorreo.test(e.target.value)) {
-//             e.target.style.borderBottom = "1px solid green";
-//         }
-//         else {
-//             e.target.style.borderBottom = "1px solid red";
-//         }
-//     }
-//     if (mensaje.value.length > 30) {
-//         e.target.style.borderBottom = "1px solid green";
-//     }
-//     else {
-//         e.target.style.borderBottom = "1px solid red";
-//     }
-// }
+
